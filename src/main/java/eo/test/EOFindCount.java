53a447e443c19e58a29c30f4259f4f882d638012
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-03-23 18:36:17 +0300

    added findCount

--- src/main/java/eo/test/EOFindCount.java
+++ src/main/java/eo/test/EOFindCount.java
@@ -1,22 +1,37 @@
 package eo.test;
 
+import eo.org.eolang.calc.EOadd;
+import eo.org.eolang.calc.EOequal;
+import eo.org.eolang.calc.EOif;
 import eo.org.eolang.core.EOObject;
 import eo.org.eolang.core.EOObjectArray;
 import eo.org.eolang.core.data.EOData;
+import eo.org.eolang.core.data.EODataObject;
+
+import java.util.stream.Stream;
 
 public class EOFindCount extends EOObject {
 
     private EOObjectArray arr;
-    private EOObject predicate;
+    private EOObject element;
 
-    public EOFindCount(EOObjectArray arr, EOObject predicate){
+    public EOFindCount(EOObjectArray arr, EOObject element){
         this.arr = arr;
-        this.predicate = predicate._setParent(this);
+        this.element = element._setParent(this);
     }
 
     @Override
     public EOData _getData() {
-        return super._getData();
-        // TO DO
+        EOObject count = Stream.of(arr.get_array())
+                .reduce(new EODataObject(0), (result, current) ->
+                        new EODataObject(
+                                new EOif(
+                                        new EOequal(current, element),
+                                        new EOadd(result, new EODataObject(1)),
+                                        new EOadd(result, new EODataObject(0))
+                                )._getData()
+                        )
+                );
+        return count._setParent(this)._getData();
     }
 }
